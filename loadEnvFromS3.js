c58d8da38d3e060324f9e4ad0bc53d6c6a3bc997
Happy Patel <patelhappy267@gmail.com>
2025-02-25 12:24:04 -0500

    remove app

--- loadEnvFromS3.js
+++ loadEnvFromS3.js
@@ -3,15 +3,12 @@ const fs = require("fs");
 const path = require("path");
 const dotenv = require("dotenv");
 
-// Configure AWS SDK
 const s3 = new AWS.S3();
 
-// Parameters
-const bucketName = "env-node-backend"; // Ensure this is your correct bucket name
+const bucketName = "env-node-backend";
 const envFileName = ".env";
 const envFilePath = path.join(__dirname, envFileName);
 
-// Function to download .env file from S3
 const downloadEnvFile = async () => {
   const params = {
     Bucket: bucketName,
@@ -20,39 +17,27 @@ const downloadEnvFile = async () => {
 
   try {
     const data = await s3.getObject(params).promise();
-    const envData = data.Body.toString();
-
-    // Log the contents of the .env file for debugging
-    console.log("Downloaded .env content:", envData); // Optional debugging step
-
-    fs.writeFileSync(envFilePath, envData);
-    console.log(
-      ".env file downloaded and written to local system successfully."
-    );
+    fs.writeFileSync(envFilePath, data.Body.toString());
+    console.log(".env file downloaded successfully");
   } catch (error) {
     console.error("Error downloading .env file:", error);
-    process.exit(1); // Exit the process with an error code if download fails
+    process.exit(1);
   }
 };
 
-// Load environment variables
 const loadEnvVariables = () => {
   if (fs.existsSync(envFilePath)) {
     dotenv.config({ path: envFilePath });
-    console.log("Environment variables loaded successfully.");
+    console.log("Environment variables loaded");
   } else {
     console.error(".env file not found");
-    process.exit(1); // Exit the process if the .env file is not found
+    process.exit(1);
   }
 };
 
-// Main function
 const main = async () => {
   await downloadEnvFile();
   loadEnvVariables();
-
-  // Start your application
-  require("./app"); // This will start the app.js file after loading the environment variables
 };
 
 main();
